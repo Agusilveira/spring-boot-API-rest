cd850026a0d44cf28b4164131b06353b443a22d9
Agustin Silveira <agusilveira14@gmail.com>
2021-01-25 20:01:34 -0300

    Second Commit

--- service-config/src/main/java/silveira/store/serviceconfig/ServiceConfigApplication.java
+++ service-config/src/main/java/silveira/store/serviceconfig/ServiceConfigApplication.java
@@ -1,11 +1,15 @@
 package silveira.store.serviceconfig;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
 @EnableConfigServer
+@SpringBootApplication
 public class ServiceConfigApplication {
 
 	public static void main(String[] args) {
