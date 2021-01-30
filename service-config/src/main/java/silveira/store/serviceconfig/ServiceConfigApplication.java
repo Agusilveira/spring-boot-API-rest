131e32bd54323c07e4755d0b45d124b0aeca9777
Agustin Silveira <agusilveira14@gmail.com>
2021-01-30 00:47:56 -0300

    Merge to master branch

--- service-config/src/main/java/silveira/store/serviceconfig/ServiceConfigApplication.java
+++ service-config/src/main/java/silveira/store/serviceconfig/ServiceConfigApplication.java
@@ -1,13 +1,9 @@
 package silveira.store.serviceconfig;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
 @EnableConfigServer
 @SpringBootApplication
 public class ServiceConfigApplication {
